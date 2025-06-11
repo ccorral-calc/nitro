1a66fb9868069363eead84dee9d96ae2fffc0e7d
Cesar Corral <cesar.corral@calculex.com>
2025-06-11 13:57:38 -0600

    install

--- externals/coda-oss/build/modules/c++/net/cmake_install.cmake
+++ externals/coda-oss/build/modules/c++/net/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/home/repos/leonardo/external/nitro_install")
+  set(CMAKE_INSTALL_PREFIX "/home/ccorral/repos/leonardo/external/nitro_install")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
